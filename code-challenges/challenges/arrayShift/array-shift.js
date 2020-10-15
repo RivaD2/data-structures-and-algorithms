e4f7d710e78f92777205136dabccd8b21fde8a5c
RivaD2 <rivkadavidowski@gmail.com>
2020-10-15 00:05:14 -0700

    wrote function insertArrayShift

--- code-challenges/challenges/arrayShift/array-shift.js
+++ code-challenges/challenges/arrayShift/array-shift.js
@@ -1,23 +1,35 @@
 'use strict'
-/*
-My function will pass in the arr and value. 
-Inside the function I will set the value the arr.length to 6. 
-I will declare a new const that holds the value I want to insert at the specific index [3]. 
-I need to return the result. 
+
+
+/*Steps:
+Create a const and assign to empty array
+Create a const and find midpoint of array using length property/2 and Math.round()
+Run a for loop on the arr
+	-Inside for loop store values from original array inside
+	- As loop runs over each index, each value would be at same index in new array
+	- Check if it is at midpoint by checking if [i] ===  midpoint then insert value
+	- To insert value, use bracket notation const empty array[i] = value
+	- if [i] < midpoint, then empty array[i] = arr[i]
+	- if [i] >= midpoint, then empty array[i + 1] =  arr[i]
+		-
+i= 0 [0, 1, 2, 3], value = 9; ---> loop runs ------> [0, 1, 9, 2, 3];  emptyArray[i] = value
 */
 
-let arr = [2, 5, 6, 8, 9];
 function insertShiftArray(arr, value) {
+    const emptyArray = [];
+    const midpoint = Math.round(arr.length/2);
+    for(let i = 0; i < arr.length; i++) {
+        if(i === midpoint) {
+            emptyArray[i] = value;
+        } if(i < midpoint) {
+            emptyArray[i] = arr[i];
+        } else {
+           (i >= midpoint)
+            emptyArray[i + 1] = arr[i];
+        }
+        return emptyArray;
+    }
 
-    console.log(arr.length);
-        let length = arr.length;
-        for(i = length/2; i >= value; i--){
-            if(i === arr.length/2) {
-                arr[length] = arr[i];
-                length--;
-            };
-            arr[value] = value;
-            }
 
 }
-insertShiftArray([1, 4, 6, 7, 8], 8);
+
