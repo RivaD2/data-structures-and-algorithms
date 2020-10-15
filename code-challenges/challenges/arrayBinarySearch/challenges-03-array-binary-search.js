7fd375fdfb6603eca6bdcbc1d4b67fb5e82d1aa6
RivaD2 <rivkadavidowski@gmail.com>
2020-10-15 14:20:17 -0700

    added testing to test.js file for challenge 3

--- code-challenges/challenges/arrayBinarySearch/challenges-03-array-binary-search.js
+++ code-challenges/challenges/arrayBinarySearch/challenges-03-array-binary-search.js
@@ -1,15 +1,36 @@
 'use strict';
 
+/*
+-My function binarySearch will have two params ( sortedArr, sKey)
+-This function should  cut my search in half each time I look for a target value.
+- Define the low end of array or start point (has to be index 0)
+-Define mid
+-(Divide) to see if sKey is greater than or less than the middleIndex or midpoint.
+-While loop should run the dividing process to find sKey
+- Inside loop, define the middleIndex or midpoint from the element in the middle of the array. To do this
+              I need to round up use Math.ceil(sortedArr length/2).
+-Define the highest point/index of sortedArr which would be sortedArr.length -1
+-In while loop check to see IF sorrtedArray at sKey[mid] is greater than or less than the middleIndex
+-If mid < sKey(target value) is less,  sKey will be to right
+-Assign our start variable as mid value to chop off the left half of the array and increment mid  by the count of 1
+-If the mid > sKey , sKey should be to left.
+-Assign our end variable to mid and chop off right of array while decrementing mid by one
+- Remove the left or right of our array from our search depending on if sKey is to right or left (Conquer).
+- If mid === sKey return sKey
+*/
 
 
 
-function bsearch (Arr,value){
-    var low  = 0 , high = Arr.length -1 ,mid ;      
-    while (low <= high){
-        mid = Math.floor((low+high)/2);     
-        if(Arr[mid]==value) return mid ; 
-        else if (Arr[mid]<value) low = mid+1;
-        else high = mid-1;          
+function binarySearch  (sortedArr, sKey){
+    var start  = 0 , end = sortedArr.length -1 , mid ;
+    while (start <= end){
+        mid = Math.floor((start + end )/2);
+        if( sortedArr[mid] === sKey ) return sKey ;
+        else if (sortedArr[mid] < sKey) start = mid + 1;
+        else end = mid - 1;
     }
     return -1 ;
 }
+binarySearch(([1, 4, 5, 6,7], 7));
+
+module.exports = binarySearch;
