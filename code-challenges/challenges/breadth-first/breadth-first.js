b4f76da63b1e8740be98988ac979976ae03143fc
RivaD2 <rivkadavidowski@gmail.com>
2020-11-04 13:20:15 -0800

    refactored findMax and working on bft method

--- code-challenges/challenges/breadth-first/breadth-first.js
+++ code-challenges/challenges/breadth-first/breadth-first.js
@@ -3,11 +3,33 @@
 const BinaryTree = require("../tree/tree");
 
 class Tree extends BinaryTree {
-    constructor() {
-        super();
+    constructor(value) {
+        super(value);
         this.max = 0;
+        this.value = value;
     }
 
-    BFT() {
+    bft(tree, callback) {
+          if(tree === null) {
+              return;
+          }
+          let queue = [tree];
+          while(queue.length > 0) {
+              let item = queue.shift();
+              let value = item.value;
+              callback(value);
+              if(item.left === null && item.right === null) {
+                  continue;
+              }
+              if(item.left !== null) {
+                  queue.push(item.left);
+              }
+              if(item.right !== null) {
+                queue.push(item.right);
+            }
+        }
     }
 }
+
+const t = new Tree(1, null, null)
+     console.log(t);
