461d1e9cc449cbcb6f820a76dd8f82d09c3b3b31
RivaD2 <rivkadavidowski@gmail.com>
2020-11-04 09:56:40 -0800

    scaffolded files for challenge 17

---
+++ code-challenges/challenges/breadth-first/breadth-first.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const BinaryTree = require("../tree/tree");
+
+class Tree extends BinaryTree {
+    constructor() {
+        super();
+        this.max = 0;
+    }
+
+    BFT() {
+    }
+}
