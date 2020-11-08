fa01eb83728188392c711533506a0c5fbb31346b
RivaD2 <rivkadavidowski@gmail.com>
2020-11-07 17:52:49 -0800

    working on fizzbuzz method

--- code-challenges/challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ code-challenges/challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -9,24 +9,46 @@ class Node {
     }
 }
 
-
 class FizzyTree extends Node {
     constructor() {
         super()
         this.root = null;
+        
     }
-
+    add(newValue) {
+        let newNode = new Node(newValue);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            //process involves looping just like in linkedlist
+            //starting in head and updating current as I go
+            let current = this.root;
+            while(current) {
+                //iterates through array until true is returned
+                this.children.some((value, index)=> {
+                    if(value === undefined) {
+                        this.children[index] = newNode;
+                        current = undefined;
+                        return true;
+                    }
+                })
+            }
+        }
+    }
 }
+
 fizzBuzzTree(tree) {
 //Is value of each node divisible by 3, 5 or both. 
-let current = this.root;
 if(current.value % 3 || current.value % 5) {
-    
+
 } 
 }
 
 
-
+const k = 3;
+const node = new Node(1, k);
+const nodeTwo = new Node(2, k)
 
 
 
