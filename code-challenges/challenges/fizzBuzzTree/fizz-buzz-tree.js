510089ba90c1019a03aef40d74dc5e943cc5c2b1
RivaD2 <rivkadavidowski@gmail.com>
2020-11-07 18:23:01 -0800

    get fizzbuzz function added and fizzbuzz tree method

--- code-challenges/challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ code-challenges/challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -3,20 +3,22 @@
 class Node {
     constructor(value, k) {
         this.value = value;
+        // I need to know what k it is so when I copy tree I can use the same num
+        this.k = k;
         //a new array of k length
         this.children = new Array(k);
 
     }
 }
 
-class FizzyTree extends Node {
+class Tree extends Node {
     constructor() {
         super()
         this.root = null;
         
     }
-    add(newValue) {
-        let newNode = new Node(newValue);
+    add(newValue, k) {
+        let newNode = new Node(newValue, k);
         if(this.root === null) {
             this.root = newNode;
             return this;
@@ -24,33 +26,82 @@ class FizzyTree extends Node {
             //process involves looping just like in linkedlist
             //starting in head and updating current as I go
             let current = this.root;
+            let queue = [];
             while(current) {
-                //iterates through array until true is returned
-                this.children.some((value, index)=> {
-                    if(value === undefined) {
+                //some() iterates through array until true is returned
+                current.children.some((child, index)=> {
+                    if(child === undefined) {
                         this.children[index] = newNode;
                         current = undefined;
                         return true;
+                    } else {
+                        queue.unshift(child);
                     }
                 })
+                if(current !== undefined) {
+                    current = queue.pop();
+                }
             }
         }
     }
 }
 
-fizzBuzzTree(tree) {
+const fizzBuzzTree = unfizzyTree => {
 //Is value of each node divisible by 3, 5 or both. 
-if(current.value % 3 || current.value % 5) {
-
+    let fizzyTree = new Tree();
+    let current = unfizzyTree.root;
+    //creating a root separately for fizzy tree before traversal of unfizzy tree
+    let fizzBuzzRoot = new Node(getFizzBuzz(current.value), current.k);
+    fizzyTree.add(fizzBuzzRoot);
+    let queue = [];
+    while(current) {
+        //some() iterates through array until true is returned
+        //looping through unfizzy tree and find all nodes to add to new tree
+        current.children.some((child, index)=> {
+            if(child === undefined) {
+                //we are at the end of the tree, nothing left to do
+                current = undefined;
+                return true;
+            } else {
+                //found a child node, so I need to add corresponding child node
+                queue.unshift(child);
+                fizzyTree.add(getFizzBuzz(current.value), current.k);
+            }
+        })
+        if(current !== undefined) {
+            current = queue.pop();
+        }
+    }
+    return fizzyTree;
 } 
+
+const getFizzBuzz = value => {
+    //this function will return value that I want to add to new tree
+    // so fizz, buzz, fizzbuzz, or original value
+    let number = parseInt(value);
+    if(number % 3 && number % 5) {
+        //If the number is divisible by 3 and 5, replace the number with “Fizzbuzz”
+        return 'Fizzbuzz';
+    } else if(number % 3) {
+        return 'Fizz';
+    } else if(number % 5) {
+        return 'Buzz';
+    } else {
+        return value;
+    }
 }
 
-
 const k = 3;
-const node = new Node(1, k);
-const nodeTwo = new Node(2, k)
+const nodeCount = 14;
+//creating a k-ary tree
+const unfizzedTree = new Tree();
+for(let i = 1; i <= nodeCount; i++) {
+    unfizzedTree.add(i.toString(), k);
+}
+console.log(JSON.stringify(unfizzedTree));
+const fizzedTree = fizzBuzzTree(unfizzedTree);
+console.log(JSON.stringify(fizzedTree));
 
 
 
-
-module.exports = FizzBuzzTree;
+module.exports = fizzBuzzTree;
