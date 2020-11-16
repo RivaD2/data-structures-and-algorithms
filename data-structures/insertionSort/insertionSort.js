31e9f361a1434fe3cff537a51ff4d930435cdb62
RivaD2 <rivkadavidowski@gmail.com>
2020-11-16 13:24:15 -0800

    modified algorithm for insertionSort

--- data-structures/insertionSort/insertionSort.js
+++ data-structures/insertionSort/insertionSort.js
@@ -4,13 +4,13 @@ let insertionSort = (arr) => {
     let length = arr.length;
     for (let i = 1; i < length; i++) {
         let key = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arrrr[j] > key) {
-            arrrr[j + 1] = arrrr[j];
-            j = j - 1;
+        let sublist = i - 1;
+        while (sublist >= 0 && arr[sublist] > key) {
+            arr[sublist + 1] = arr[sublist];
+            sublist = sublist - 1;
         }
-        arrrr[j + 1] = key;
+        arr[sublist + 1] = key;
     }
-    return arrrr;
+    return arr;
 };
 console.log(insertionSort([0, 15, 3, 18, 10]));
