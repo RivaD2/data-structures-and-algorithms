a1c72330ab5afb61e9f04cf1eda87bd561696408
RivaD2 <rivkadavidowski@gmail.com>
2020-11-30 13:41:57 -0800

    added whiteboard image

--- data-structures/graph/graph.js
+++ data-structures/graph/graph.js
@@ -7,4 +7,37 @@ class Graph {
     addNode(vertex) {
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    getNeighbors(startingNode) {
+        let visited = {};
+        let stack = [startingNode];
+        const result = [];
+        let currentNode;
+
+        visited[startingNode] = true;
+        while(stack.length) {
+            currentNode = stack.pop();
+            result.push(currentNode);
+           this.adjacencyList[currentNode].forEach(neighbor => {
+               if(!visited[neighbor]) {
+                   visited[neighbor] = true;
+                   stack.push(neighbor);
+               }
+           })
+        }
+        return result;
+    }
 }
+
+let g = new Graph();
+g.addNode('A');
+g.addNode('B');
+g.addNode('C');
+g.addNode('D');
+g.addEdge('A', 'B');
+g.addEdge('A', 'D');
+g.addEdge('B', 'C');
+g.getNeighbors('A');
